e85e462dd10d712938da8b90b140ef115f29da07
Yacine Bouziane <bouziyacine21@gmail.com>
2025-11-29 15:26:42 -0500

    Changes to be committed: 	modified:   client/src/pages/Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -24,7 +24,7 @@ export default function Login() {
     }
     const data = await res.json();
     setUser(data.user);
-    window.location.href = "/home";
+    window.location.href = "/dashboard";
   }
 
   return (
