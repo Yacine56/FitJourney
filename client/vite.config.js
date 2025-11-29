cdcaa4521729d137ab1aaac1becd35b832de236f
Yacine Bouziane <bouziyacine21@gmail.com>
2025-11-28 22:27:33 -0500

    Changes to be committed: 	modified:   client/vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const backend = process.env.VITE_API_URL || "http://localhost:5000";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: backend,
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
+});
